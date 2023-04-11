3a99d5811469a0c4938c8286d53b74a3d1e2e6f5
devCarlosALexandre <carloalexandresousa@gmail.com>
2023-04-11 11:34:26 -0300

    utilizando getx no controller

--- lib/app/controllers/categories_controller.dart
+++ lib/app/controllers/categories_controller.dart
@@ -1,15 +1,22 @@
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:my_lyst/app/models/category/category_model.dart';
 import 'package:my_lyst/app/respositories/category_repository.dart';
 
-class CategoryController {
+class CategoryController extends GetxController {
   final category = CategoryRepository();
+
+  RxList<CategoryModel> listCategories = RxList<CategoryModel>([]);
   RxBool isLoading = false.obs;
 
-  Future<List<CategoryModel>> getCategories() async {
+  Future getCategories() async {
     isLoading.value = true;
     List<CategoryModel> result = await category.getAll();
+    if (result.isNotEmpty) {
+      listCategories.assignAll(result);
+    } else {
+      print("Erro ao retornar os dados");
+    }
     isLoading.value = false;
-    return result;
   }
 }
--- lib/app/pages/category/category_page.dart
+++ lib/app/pages/category/category_page.dart
@@ -1,51 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:my_lyst/app/models/category/category_model.dart';
+import 'package:get/get.dart';
 
 import '../../controllers/categories_controller.dart';
 
-class CategoryPage extends StatefulWidget {
-  const CategoryPage({super.key});
-
-  @override
-  State<CategoryPage> createState() => _CategoryPageState();
-}
-
-class _CategoryPageState extends State<CategoryPage> {
-  List<CategoryModel> categorires = [];
-  CategoryController controller = CategoryController();
-  _init() async {
-    List<CategoryModel> response = await controller.getCategories();
-
-    setState(() {
-      categorires = response;
-    });
-  }
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _init();
-  }
+class CategoryPage extends StatelessWidget {
+  CategoryPage({super.key});
+  final controller = Get.put(CategoryController());
 
   @override
   Widget build(BuildContext context) {
+    controller.getCategories();
     return Scaffold(
         appBar: AppBar(
           title: const Text('Category'),
         ),
-        body: categorires.isNotEmpty
-            ? SingleChildScrollView(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  primary: false,
-                  itemBuilder: (context, index) => Card(
-                      child: ListTile(
-                          title: Text(categorires[index].name ?? "",
-                              style: TextStyle()))),
-                  itemCount: categorires.length,
-                ),
-              )
-            : const Center(child: CircularProgressIndicator()));
+        body: Obx(() {
+          if (controller.isLoading.value) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            return ListView.builder(
+              itemBuilder: (context, index) => Card(
+                  child: ListTile(
+                      title: Text(
+                controller.listCategories[index].name ?? "",
+              ))),
+              itemCount: controller.listCategories.length,
+            );
+          }
+        }));
   }
 }
