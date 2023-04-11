8acdcc688685933b99d22599ef0d598cfbc00c57
devCarlosALexandre <carloalexandresousa@gmail.com>
2023-04-11 11:46:39 -0300

    utilizando getx no controller do usuario

--- lib/app/controllers/user_controller.dart
+++ lib/app/controllers/user_controller.dart
@@ -1,16 +1,21 @@
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:my_lyst/app/models/user/user_model.dart';
 import 'package:my_lyst/app/respositories/user_repository.dart';
 
-class UserController {
+class UserController extends GetxController {
   final user = UserRepository();
-  RxList<UserModel> listCategories = RxList<UserModel>([]);
+  RxList<UserModel> listUsers = RxList<UserModel>([]);
   RxBool isLoading = false.obs;
 
-  Future<List<UserModel>> getUsers() async {
+  Future getUsers() async {
     isLoading.value = true;
     List<UserModel> result = await user.getAll();
+    if (result.isNotEmpty) {
+      listUsers.assignAll(result);
+    } else {
+      print("Erro ao retornar usuarios");
+    }
     isLoading.value = false;
-    return result;
   }
 }
--- lib/app/pages/profile/profile_page.dart
+++ lib/app/pages/profile/profile_page.dart
@@ -1,66 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:my_lyst/app/controllers/user_controller.dart';
 
-import '../../controllers/user_controller.dart';
-import '../../models/user/user_model.dart';
-
-class UserPage extends StatefulWidget {
-  const UserPage({super.key});
-
-  @override
-  State<UserPage> createState() => _UserPageState();
-}
-
-class _UserPageState extends State<UserPage> {
-  List<UserModel> users = [];
-
-  UserController controller = UserController();
-
-  _init() async {
-    List<UserModel> response = await controller.getUsers();
-    setState(() {
-      users = response;
-    });
-  }
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _init();
-  }
+class UserPage extends StatelessWidget {
+  UserPage({super.key});
+  final controller = Get.put(UserController());
 
   @override
   Widget build(BuildContext context) {
+    controller.getUsers();
     return Scaffold(
       appBar: AppBar(
         title: const Text('User'),
       ),
-      body: users.isNotEmpty
-          ? ListView.separated(
-              padding: const EdgeInsets.all(0),
-              itemBuilder: (context, index) => ListTile(
-                    title: Text(users[index].name ?? ""),
-                    subtitle: Text(users[index].email ?? ""),
-                  ),
-              separatorBuilder: (context, index) => const Divider(),
-              itemCount: users.length)
-          : const Center(child: CircularProgressIndicator()),
-    );
-  }
-}
-
-class User {
-  final int id;
-  final String name;
-  final String email;
-
-  User({required this.id, required this.name, required this.email});
-
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(
-      id: json['id'],
-      name: json['name'],
-      email: json['email'],
+      body: GetX<UserController>(
+        builder: (_) {
+          if (controller.isLoading.value) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            return ListView.separated(
+                padding: const EdgeInsets.all(0),
+                itemBuilder: (context, index) => ListTile(
+                      title: Text(controller.listUsers[index].name ?? ""),
+                      subtitle: Text(controller.listUsers[index].email ?? ""),
+                    ),
+                separatorBuilder: (context, index) => const Divider(),
+                itemCount: controller.listUsers.length);
+          }
+        },
+      ),
     );
   }
 }
